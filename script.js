f9e27f52ac1741c6e6c7bc4b80a8611cd4d212bb
Suraj Patnaikuni <79284315+PatnSuraj@users.noreply.github.com>
2021-06-13 11:19:12 -0300

    Two circle intersection

---
+++ script.js
@@ -0,0 +1,13 @@
+function clickToIntersect() {
+    document.getElementById("Circle_1").style.marginLeft = "580px";
+    document.getElementById("Circle_2").style.marginLeft = "700px";
+    document.getElementById("text_names_1").style.visibility = "hidden";
+    document.getElementById("text_names_2").style.visibility = "hidden";
+    document.getElementById("Circle_1").style.o = "0.6";
+    document.getElementById("Circle_2").style.opacity = "0.6";
+    document.getElementById("ins_button").style.backgroundColor = "#B9BFC7";
+    document.getElementById("ins_button").style.border = "none";
+    document.getElementById("ins_button").style.color = "black";
+    document.getElementById("ins_button").style.fontSize = "20px";
+    document.getElementById("ins_button").style.fontWeight = "normal";
+  }
